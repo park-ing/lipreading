eb45cc5ed41bc8ed6e201c4d0b691fcb48bb7256
park-ing <yh.park8742@gmail.com>
2022-04-01 21:20:19 +0900

    0401

--- TSM_resnet18.py
+++ TSM_resnet18.py
@@ -1,7 +1,6 @@
 import math
 import torch
 import torch.nn as nn
-import pdb
 import torch.nn.functional as F
 
 
@@ -126,109 +125,103 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
+
+    def make_temporal_shift(net, n_segment, n_div=8):
+    
+        n_segment_list = [n_segment] * 4
+    
+        assert n_segment_list[-1] > 0
+        print('=> n_segment per stage: {}'.format(n_segment_list))
+
+        def make_block_temporal(stage, this_segment):
+            blocks = list(stage.children())
+            print('=> Processing stage with {} blocks'.format(len(blocks)))
+            for i, b in enumerate(blocks):
+                blocks[i] = TemporalShift(b, n_segment=this_segment, n_div=n_div)
+            return nn.Sequential(*(blocks))
+
+        net.layer1 = make_block_temporal(net.layer1, n_segment_list[0])
+        net.layer2 = make_block_temporal(net.layer2, n_segment_list[1])
+        net.layer3 = make_block_temporal(net.layer3, n_segment_list[2])
+        net.layer4 = make_block_temporal(net.layer4, n_segment_list[3])
+    
+
+
+
+    
     def forward(self, x):
         ## test size resize    
         x = self.conv1(x)
         x = self.bn1(x)
         x = F.relu(x)
         x = self.maxpool1(x)
-        #3
+        ##
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
+        
         x = self.linear(x)
+        
         return x
 
 
 
+## temporal shift
+class TemporalShift(nn.Module):
+    def __init__(self, net, n_segment=3, n_div=8, inplace=False):
+        super(TemporalShift, self).__init__()
+        self.net = net
+        self.n_segment = n_segment
+        self.fold_div = n_div
+        self.inplace = inplace
+        if inplace:
+            print('=> Using in-place shift...')
+        print('=> Using fold div: {}'.format(self.fold_div))
 
-
-
-
-'''
-# Residual Block 구조 정의
-class BasicBlock(nn.Module):
-    mul = 1
-    def __init__(self, in_planes, out_planes, stride=1):
-        super(BasicBlock, self).__init__()
-        
-        # stride를 통해 너비와 높이 조정
-        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_planes)
-        
-        # stride = 1, padding = 1이므로, 너비와 높이는 항시 유지됨
-        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_planes)
-        
-        # x를 그대로 더해주기 위함
-        self.shortcut = nn.Sequential()
-        
-        # 만약 size가 안맞아 합연산이 불가하다면, 연산 가능하도록 모양을 맞춰줌
-        if stride != 1: # x와 
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_planes)
-            )
-    
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = F.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out += self.shortcut(x) # 필요에 따라 layer를 Skip
-        out = F.relu(out)
-        return out
-    
-        
-        
+        x = self.shift(x, self.n_segment, fold_div=self.fold_div, inplace=self.inplace)
+        return self.net(x)
 
+    @staticmethod
+    def shift(x, n_segment, fold_div=3, inplace=False):
+        nt, c, h, w = x.size()
+        n_batch = nt // n_segment
+        x = x.view(n_batch, n_segment, c, h, w)
+
+        fold = c // fold_div
+        if inplace:
+            # Due to some out of order error when performing parallel computing. 
+            # May need to write a CUDA kernel.
+            raise NotImplementedError  
+            # out = InplaceShift.apply(x, fold)
+        else:
+            out = torch.zeros_like(x)
+            out[:, :-1, :fold] = x[:, 1:, :fold]  # shift left
+            out[:, 1:, fold: 2 * fold] = x[:, :-1, fold: 2 * fold]  # shift right
+            out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # not shift
+
+        return out.view(nt, c, h, w)
+
+
+def make_temporal_shift(net, n_segment, n_div=8):
     
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        #RGB 3개채널에서 64개의 Kernel 사용
-        self.in_planes = 64
-        
-        # Resnet 논문 구조 그대로 구현
-        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7, stride=2, padding = 3)
-        self.bn1 = nn.BatchNorm2d(self.in_planes)
-        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        
-        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.linear = nn.Linear(512 * block.mul, num_classes)
-        
-    def make_layer(self, block, out_planes, num_blocks, stride):
-        # layer 앞부분에서만 크기를 절반으로 줄이므로, 아래와 같은 구조
-        strides = [stride] + [1] * (num_blocks-1)
-        layers = []
-        for i in range(num_blocks):
-            layers.append(block(self.in_planes, out_planes, strides[i]))
-            self.in_planes = block.mul * out_planes
-        return nn.Sequential(*layers)
+    n_segment_list = [n_segment] * 4
     
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = F.relu(out)
-        out = self.maxpool1(out)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.avgpool(out)
-        out = torch.flatten(out,1)
-        out = self.linear(out)
-        return out
-    
-    
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
-'''
+    assert n_segment_list[-1] > 0
+    print('=> n_segment per stage: {}'.format(n_segment_list))
+
+    def make_block_temporal(stage, this_segment):
+                blocks = list(stage.children())
+                print('=> Processing stage with {} blocks'.format(len(blocks)))
+                for i, b in enumerate(blocks):
+                    blocks[i] = TemporalShift(b, n_segment=this_segment, n_div=n_div)
+                return nn.Sequential(*(blocks))
+
+    net.layer1 = make_block_temporal(net.layer1, n_segment_list[0])
+    net.layer2 = make_block_temporal(net.layer2, n_segment_list[1])
+    net.layer3 = make_block_temporal(net.layer3, n_segment_list[2])
+    net.layer4 = make_block_temporal(net.layer4, n_segment_list[3])
+
--- shape_test.py
+++ shape_test.py
@@ -7,10 +7,53 @@ import torch.nn as nn
 conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding = 3)
 bn1 = nn.BatchNorm2d(64)
 maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-input = torch.randn(256, 3, 32, 32)
-output = conv1(input)
-print(output.shape)
-output2 = bn1(output)
-print(output2.shape)
-output3 = maxpool1(output2)
-print(output3.shape)
+#input = torch.randn(64, 3, 32, 32)
+
+#x = input
+#nt, c, h, w = x.size()
+#n_batch = nt // 3
+##x = x.view(n_batch, 3, c, h, w)
+#output = conv1(input)
+#print(output.shape)
+#output2 = bn1(output)
+#print(output2.shape)
+#output3 = maxpool1(output2)
+#print(output3.shape)
+
+x = torch.randn(4, 3, 5, 5)
+print(x)
+nt, c, h, w = x.size()
+n_batch = nt // 2
+x = x.view(-1, 2, c, h, w)
+out = torch.zeros_like(x)
+fold = 1
+out[:, :-1, :fold] = x[:, 1:, :fold]  # shift left
+out[:, 1:, fold: 2 * fold] = x[:, :-1, fold: 2 * fold]  # shift right
+out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # not shift
+print('shift: {}'.format(out.shape))
+print(out)
+
+
+
+def shift(x, n_segment, fold_div=3, inplace=False):
+    nt, c, h, w = x.size()
+    n_batch = nt // n_segment
+    #x = x.view(n_batch, n_segment, c, h, w)
+
+    fold = c // fold_div
+    if inplace:
+        # Due to some out of order error when performing parallel computing. 
+        # May need to write a CUDA kernel.
+        raise NotImplementedError  
+        # out = InplaceShift.apply(x, fold)
+    else:
+        out = torch.zeros_like(x)
+        out[:, :-1, :fold] = x[:, 1:, :fold]  # shift left
+        out[:, 1:, fold: 2 * fold] = x[:, :-1, fold: 2 * fold]  # shift right
+        out[:, :, 2 * fold:] = x[:, :, 2 * fold:]  # not shift
+
+    return out.view(nt, c, h, w)
+
+#print(input.shape)
+#x = shift(input,n_segment=3,fold_div=8,inplace=False)
+#print(x.shape)
